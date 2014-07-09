edb3a17c10ef4655bd644e478d69c6da4ddc5faf
Alex Foster <alexjfno1@gmail.com>
2014-07-09 14:12:04 +0100

    Making the search work!!

--- app/js/shorts_weather.js
+++ app/js/shorts_weather.js
@@ -3,29 +3,50 @@ $(document).ready(function(){
 });
 
 function setup() {
-  navigator.geolocation.getCurrentPosition(function(position){
+  $(".search-button").click(function(e) {
+    e.preventDefault();
+    // Disable and replace button with spinner
+    var searchTerm = $(".search-box").val();
     $.ajax({
-      url: "/shorts-weather?lat=" + position.coords.latitude + "&long=" + position.coords.longitude,
+      url: "/shorts-weather/get-position?address=" + encodeURIComponent(searchTerm),
       type: "get"
-    }).done(function(response){
-      $(".search-box").val(response.address);
-      $(".search-address").show();
-      $(".search-box").focus();
-      if (response.shorts_weather) {
-        setShortsWeather();
-      } else {
-        setNotShortsWeather();
-      }
+    }).done(function(response) {
+      checkIfItIsShortsWeather(response.latitude, response.longitude);
     });
+  });
+  navigator.geolocation.getCurrentPosition(function(position) {
+    checkIfItIsShortsWeather(position.coords.latitude, position.coords.longitude);
   })
 }
 
+function checkIfItIsShortsWeather(latitude, longitude) {
+  $.ajax({
+    url: "/shorts-weather?lat=" + latitude + "&long=" + longitude,
+    type: "get"
+  }).done(function(response){
+    $(".search-box").val(response.address);
+    $(".search-address").show();
+    if (response.is_it_shorts_weather) {
+      setShortsWeather();
+    } else {
+      setNotShortsWeather();
+    }
+  });
+}
+
 function setShortsWeather() {
+  clearState();
   $(".answer").addClass("yes");
   $(".answer").html("Yep!");
 }
 
 function setNotShortsWeather() {
+  clearState();
   $(".answer").addClass("no");
   $(".answer").html("Nope");
+}
+
+function clearState() {
+  $(".answer").removeClass("yes")
+  $(".answer").removeClass("no")
 }
