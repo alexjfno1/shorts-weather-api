aa67d8180d035a337c2b72a84758fe4852de23fa
Alex Foster <alexjfno1@gmail.com>
2014-07-09 13:02:33 +0100

    Logging faraday requests

--- lib/google_maps.rb
+++ lib/google_maps.rb
@@ -18,6 +18,7 @@ class GoogleMaps
   def call_api lat, long
     google_maps_api = Faraday.new(url: "http://maps.googleapis.com/maps/api/geocode") do |faraday|
       faraday.adapter Faraday.default_adapter
+      faraday.response :logger
     end
     google_maps_api.get("json?latlng=#{lat},#{long}&sensor=false")
   end
