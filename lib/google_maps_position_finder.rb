e15ff69e36dea849bcf79edf81335df200133c30
Alex Foster <alexjfno1@gmail.com>
2014-07-09 08:32:54 +0100

    Removing URL encoding when getting coordinates from google as address will already be encoded

--- lib/google_maps_position_finder.rb
+++ lib/google_maps_position_finder.rb
@@ -22,7 +22,7 @@ class GoogleMapsPositionFinder
     position_finder = Faraday.new(url: "http://maps.googleapis.com/maps/api/geocode") do |faraday|
       faraday.adapter Faraday.default_adapter
     end
-    position_finder.get("json?address=#{URI::encode(address)}")
+    position_finder.get("json?address=#{address}")
   end
 
   def position
